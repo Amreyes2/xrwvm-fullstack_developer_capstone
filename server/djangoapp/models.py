e9d0468de57aa6745b2f9abc0a03ab03a08f3b48
Adolfo Martin <amreyes.2@gmail>
2024-10-23 17:39:50 -0600

    errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -13,14 +13,12 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 class CarMake(models.Model):
-    name =  models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     description = models.TextField
 
     def __str__(self):
         return self.name
     
-
-
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
@@ -30,17 +28,19 @@ class CarMake(models.Model):
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+
 class CarModel(models.Model):
-    car_make= models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    CAR_TYPES= [
-        ('SEDAN','sedan'),
+    CAR_TYPES = [
+        ('SEDAN', 'sedan'),
         ('SUV', 'suv'),
-        ('WAGON','wagon'),
+        ('WAGON', 'wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
+        validators = [
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
